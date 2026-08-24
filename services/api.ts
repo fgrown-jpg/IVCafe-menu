106c0e978107ea49e3e18cb8d95ed41173f567e9
fgrown.jpg <f.z.bozorg1@gmail.com>
2026-08-25 00:13:22 +0330

    read products from local db.json instead of localhost API

--- services/api.ts
+++ services/api.ts
@@ -1,15 +1,24 @@
-import axios from "axios"
-const client= axios.create({
-    baseURL:"http://localhost:3000/"
+// import axios from "axios"
+// const client= axios.create({
+//     baseURL:"http://localhost:3000/"
 
-})
-export async function getProduct() {
-    const {data}=await client ("/products")
-    return data
+// })
+// export async function getProduct() {
+//     const {data}=await client ("/products")
+//     return data
     
+// }
+
+// export async function getItem(id:string | number) {
+//     const {data}=await client(`/products/${id}`)
+//     return data
+// }
+import db from "../data/db.json";
+
+export async function getProduct() {
+  return db.products;
 }
 
-export async function getItem(id:string | number) {
-    const {data}=await client(`/products/${id}`)
-    return data
+export async function getItem(id: string | number) {
+  return db.products.find((p) => p.id === String(id));
 }
